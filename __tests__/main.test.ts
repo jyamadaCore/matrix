fa23933161754d2dba6d7d079448fff70cea9cca
Judy Lin <judy@corellium.com>
2024-03-20 14:29:04 -0500

    action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,76 +1,291 @@
-/**
- * Unit tests for the action's main functionality, src/main.ts
- *
- * These should be run as if the action was called from a workflow.
- * Specifically, the inputs listed in `action.yml` should be set as environment
- * variables following the pattern `INPUT_<INPUT_NAME>`.
- */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as artifact from '@actions/artifact';
+import * as path from 'path';
+import { mocked } from 'jest-mock';
+import { readFileSync, unlinkSync } from 'fs';
 import * as main from '../src/main';
 
-// Mock the action's main function
+// Unit tests for the action's main functionality, src/main.ts
 const runMock = jest.spyOn(main, 'run');
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/;
+jest.mock('@actions/artifact');
+jest.mock('fs', () => {
+  const actual = jest.requireActual('fs');
+  return {
+    ...actual,
+    promises: { access: jest.fn() },
+    readFileSync: jest.fn(),
+  };
+});
 
-// Mock the GitHub Actions core library
-let debugMock: jest.SpiedFunction<typeof core.debug>;
+// Mock the GitHub Actions libraries
 let errorMock: jest.SpiedFunction<typeof core.error>;
 let getInputMock: jest.SpiedFunction<typeof core.getInput>;
 let setFailedMock: jest.SpiedFunction<typeof core.setFailed>;
 let setOutputMock: jest.SpiedFunction<typeof core.setOutput>;
 
 describe('action', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
+  describe('run', () => {
+    const mockUrl = 'https://www.website.com';
+    const validateExecCall = async (actual: string, expected: string): Promise<void> => {
+      expect(actual).toEqual(expect.stringContaining(expected));
+    };
 
-    debugMock = jest.spyOn(core, 'debug').mockImplementation();
-    errorMock = jest.spyOn(core, 'error').mockImplementation();
-    getInputMock = jest.spyOn(core, 'getInput').mockImplementation();
-    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation();
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation();
-  });
+    beforeEach(() => {
+      jest.resetModules();
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return '500';
-        default:
-          return '';
-      }
+      errorMock = jest.spyOn(core, 'error').mockImplementation();
+      getInputMock = jest.spyOn(core, 'getInput').mockImplementation();
+      setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation();
+      setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation();
+      mocked(readFileSync).mockReturnValueOnce(JSON.stringify([{ wait: 100 }, { duration: 400 }]));
+      process.env.PROJECT = 'mockProjectId';
+      process.env.API_TOKEN = 'mockApiToken';
+      process.env.GITHUB_WORKSPACE = path.join(__dirname, '.');
     });
 
-    await main.run();
-    expect(runMock).toHaveReturned();
-
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...');
-    expect(debugMock).toHaveBeenNthCalledWith(2, expect.stringMatching(timeRegex));
-    expect(debugMock).toHaveBeenNthCalledWith(3, expect.stringMatching(timeRegex));
-    expect(setOutputMock).toHaveBeenNthCalledWith(1, 'time', expect.stringMatching(timeRegex));
-    expect(errorMock).not.toHaveBeenCalled();
-  });
-
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number';
-        default:
-          return '';
-      }
+    afterEach(async () => {
+      jest.clearAllMocks();
     });
 
-    await main.run();
-    expect(runMock).toHaveReturned();
+    it(`should throw an error if 'PROJECT' secret is missing`, async () => {
+      delete process.env.PROJECT;
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(1, 'milliseconds not a number');
-    expect(errorMock).not.toHaveBeenCalled();
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Environment secret missing: PROJECT');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'API_TOKEN' secret is missing`, async () => {
+      delete process.env.API_TOKEN;
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Environment secret missing: API_TOKEN');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'flavor' input is missing`, async () => {
+      getInputMock.mockImplementation(name => (name === 'flavor' ? '' : 'mockVal'));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Input required and not supplied: flavor');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'os' input is missing`, async () => {
+      getInputMock.mockImplementation(name => (name === 'os' ? '' : 'mockVal'));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Input required and not supplied: os');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'appUrl' input is missing`, async () => {
+      getInputMock.mockImplementation(name => (name === 'appUrl' ? '' : 'mockVal'));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Input required and not supplied: appUrl');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'appUrl' input is invalid`, async () => {
+      getInputMock.mockImplementation(name => (name === 'appUrl' ? 'notAUrl' : mockUrl));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Provided URL is invalid: appUrl');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'inputUrl' input is missing`, async () => {
+      getInputMock.mockImplementation(name => (name === 'inputUrl' ? '' : 'mockVal'));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Input required and not supplied: inputUrl');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'inputUrl' input is invalid`, async () => {
+      getInputMock.mockImplementation(name => (name === 'inputUrl' ? 'notAUrl' : mockUrl));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Provided URL is invalid: inputUrl');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it(`should throw an error if 'wordlistUrl' exists and is invalid`, async () => {
+      getInputMock.mockImplementation(name => (name === 'wordlistUrl' ? 'notAUrl' : mockUrl));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(setFailedMock).toHaveBeenCalledWith('Provided URL is invalid: wordlistUrl');
+      expect(errorMock).not.toHaveBeenCalled();
+    });
+
+    it('should execute automated MAST', async () => {
+      const workspaceDir = process.env.GITHUB_WORKSPACE as string;
+      const reportArtifactPath = 'mockDownloadPath';
+      const instanceId = 'mockInstanceId';
+      const bundleId = 'com.android.egg';
+      const wordlistId = 'wordlistId';
+      getInputMock.mockImplementation(input => (input.includes('Url') ? mockUrl : 'mockVal'));
+
+      const execSpy = jest.spyOn(exec, 'exec');
+      execSpy
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(instanceId));
+          return 0;
+        })
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        // getBundleId
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify([{ applicationType: 'User', bundleID: bundleId }])));
+          return 0;
+        })
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        // uploadWordlistFile
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify([{ id: wordlistId }])));
+          return 0;
+        })
+        // running mast
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ id: 'mockAssessmentId' })));
+          return 0;
+        })
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'new' })));
+          return 0;
+        })
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'monitoring' })));
+          return 0;
+        })
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'readyForTesting' })));
+          return 0;
+        })
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'complete' })));
+          return 0;
+        })
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from('mastReport'));
+          return 0;
+        })
+        // cleanup
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async () => Promise.resolve(0))
+        .mockImplementationOnce(async () => Promise.resolve(0));
+
+      jest.spyOn(artifact, 'DefaultArtifactClient').mockImplementation();
+      const uploadArtifactMock = jest
+        .spyOn(artifact.DefaultArtifactClient.prototype, 'uploadArtifact')
+        .mockImplementation(async () => Promise.resolve({ id: 1234 }));
+      const downloadArtifactMock = jest
+        .spyOn(artifact.DefaultArtifactClient.prototype, 'downloadArtifact')
+        .mockImplementation(async () => Promise.resolve({ downloadPath: reportArtifactPath }));
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+      expect(uploadArtifactMock).toHaveBeenCalled();
+      expect(downloadArtifactMock).toHaveBeenCalled();
+      expect(setOutputMock).toHaveBeenNthCalledWith(1, 'report', expect.stringMatching(reportArtifactPath));
+
+      validateExecCall(execSpy.mock.calls[0][0], 'npm install -g @corellium/corellium-cli');
+      validateExecCall(execSpy.mock.calls[1][0], 'corellium login --endpoint');
+      validateExecCall(execSpy.mock.calls[2][0], 'corellium instance create');
+      validateExecCall(execSpy.mock.calls[3][0], `curl -L -o ${path.join(workspaceDir, 'appFile')} ${mockUrl}`);
+      validateExecCall(execSpy.mock.calls[4][0], 'corellium apps install --project');
+      validateExecCall(execSpy.mock.calls[5][0], 'corellium apps --project');
+      validateExecCall(execSpy.mock.calls[6][0], `curl -L -o ${path.join(workspaceDir, 'wordlist.txt')} ${mockUrl}`);
+      validateExecCall(execSpy.mock.calls[7][0], `curl -L -o ${path.join(workspaceDir, 'inputs.json')} ${mockUrl}`);
+      validateExecCall(execSpy.mock.calls[8][0], 'corellium image create --project');
+      validateExecCall(
+        execSpy.mock.calls[9][0],
+        `corellium mast create-assessment --instance ${instanceId} --bundle ${bundleId} --wordlist ${wordlistId}`,
+      );
+      validateExecCall(execSpy.mock.calls[10][0], `corellium mast get-assessment --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[11][0], `corellium mast start-monitor --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[12][0], `corellium mast get-assessment --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[13][0], `corellium input ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[14][0], `corellium mast stop-monitor --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[15][0], `corellium mast get-assessment --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[16][0], `corellium mast test --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[17][0], `corellium mast get-assessment --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[18][0], `corellium mast download-report --instance ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[19][0], `corellium instance stop ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[20][0], `corellium instance delete ${instanceId}`);
+      validateExecCall(execSpy.mock.calls[21][0], 'corellium logout');
+
+      unlinkSync(path.join(__dirname, './report.html'));
+    }, 15000);
+  });
+
+  describe('pollAssessmentForStatus', () => {
+    beforeEach(() => {
+      jest.resetModules();
+    });
+
+    afterEach(async () => {
+      jest.clearAllMocks();
+    });
+
+    it('should throw an error if the assessment goes into a failed state', async () => {
+      const execSpy = jest.spyOn(exec, 'exec');
+      execSpy
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'testing' })));
+          return 0;
+        })
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'failed' })));
+          return 0;
+        });
+
+      await expect(main.pollAssessmentForStatus('mockAssessmentId', 'mockInstanceId', 'complete')).rejects.toThrow(
+        'MAST automated test failed!',
+      );
+      expect(execSpy.mock.calls.length).toBe(2);
+    });
+
+    it('should keep polling until desired state has been reached', async () => {
+      const execSpy = jest.spyOn(exec, 'exec');
+      execSpy
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'testing' })));
+          return 0;
+        })
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'testing' })));
+          return 0;
+        })
+        .mockImplementationOnce(async (_command, _args, options: any) => {
+          options.listeners.stdout(Buffer.from(JSON.stringify({ status: 'readyForTesting' })));
+          return 0;
+        });
+
+      const resp = await main.pollAssessmentForStatus('mockAssessmentId', 'mockInstanceId', 'readyForTesting');
+      expect(resp).toEqual('readyForTesting');
+      expect(execSpy.mock.calls.length).toBe(3);
+    }, 10000);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import { exec } from '@actions/exec';
+import { DefaultArtifactClient } from '@actions/artifact';
+import * as path from 'path';
+import fs from 'fs';
 
 /**
  * The main function for the action.
@@ -7,20 +10,256 @@ import { wait } from './wait';
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`);
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString());
+    validateInputsAndVars();
+    await installCorelliumCli();
+    const instanceId = await setupDevice();
+    const report = await runMast(instanceId);
+    await cleanup(instanceId);
+    await storeReportInArtifacts(report);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
+}
+
+async function installCorelliumCli(): Promise<void> {
+  core.info('Installing Corellium-CLI...');
+  await exec('npm install -g @corellium/corellium-cli');
+  await execCmd(`corellium login --endpoint ${core.getInput('server')} --apitoken ${process.env.API_TOKEN}`);
+}
+
+async function setupDevice(): Promise<string> {
+  const projectId = process.env.PROJECT;
+
+  core.info('Creating device...');
+  const resp = await execCmd(
+    `corellium instance create ${core.getInput('flavor')} ${core.getInput('os')} ${projectId} --wait`,
+  );
+  const instanceId = resp?.toString().trim();
+
+  core.info('Downloading app...');
+  const appPath = await downloadFile('appFile', core.getInput('appUrl'));
+
+  core.info(`Installing app on ${instanceId}...`);
+  await execCmd(`corellium apps install --project ${projectId} --instance ${instanceId} --app ${appPath}`);
+
+  return instanceId;
+}
+
+async function runMast(instanceId: string): Promise<string> {
+  const [bundleId, wordlistId, inputInfo] = await Promise.all([
+    getBundleId(instanceId),
+    uploadWordlistFile(instanceId),
+    downloadInputFile(),
+  ]);
+
+  const inputsFilePath = inputInfo.inputsFilePath;
+  const inputsTimeout = inputInfo.inputsTimeout;
+
+  core.info('Running MAST...');
+
+  core.info('Creating assessment...');
+  let assessmentId: string | undefined;
+  try {
+    let createAssessment = `corellium mast create-assessment --instance ${instanceId} --bundle ${bundleId}`;
+    if (wordlistId) {
+      createAssessment += ` --wordlist ${wordlistId}`;
+    }
+    const resp = await execCmd(createAssessment);
+    assessmentId = (tryJsonParse(resp) as { id: string })?.id;
+    core.info(`Created assessment ${assessmentId}...`);
+  } catch (err) {
+    throw new Error(`Error creating MAST assessment! err=${err}`);
+  }
+
+  await pollAssessmentForStatus(assessmentId, instanceId, 'new');
+  core.info('Starting monitor...');
+  await execCmd(`corellium mast start-monitor --instance ${instanceId} --assessment ${assessmentId}`);
+
+  await pollAssessmentForStatus(assessmentId, instanceId, 'monitoring');
+  core.info('Executing inputs on device...');
+  await execCmd(`corellium input ${instanceId} ${inputsFilePath}`);
+  core.info(`Waiting ${inputsTimeout}ms for inputs to execute...`);
+  await wait(inputsTimeout);
+
+  core.info('Stopping monitor...');
+  await execCmd(`corellium mast stop-monitor --instance ${instanceId} --assessment ${assessmentId}`);
+
+  await pollAssessmentForStatus(assessmentId, instanceId, 'readyForTesting');
+  core.info('Executing tests...');
+  await execCmd(`corellium mast test --instance ${instanceId} --assessment ${assessmentId}`);
+
+  await pollAssessmentForStatus(assessmentId, instanceId, 'complete');
+  core.info('Downloading assessment...');
+  return await execCmd(`corellium mast download-report --instance ${instanceId} --assessment ${assessmentId}`);
+}
+
+async function cleanup(instanceId: string): Promise<void> {
+  core.info('Cleaning up...');
+  await execCmd(`corellium instance stop ${instanceId}`);
+  await execCmd(`corellium instance delete ${instanceId}`);
+  await execCmd(`corellium logout`);
+}
+
+async function getBundleId(instanceId: string): Promise<string> {
+  const resp = await execCmd(`corellium apps --project ${process.env.PROJECT} --instance ${instanceId}`);
+  const appList = tryJsonParse(resp) as unknown as { applicationType: string; bundleID: string }[];
+  const bundleId = appList?.find(app => app.applicationType === 'User')?.bundleID;
+  if (!bundleId) {
+    throw new Error('Error getting bundleId!');
+  }
+  return bundleId;
+}
+
+async function uploadWordlistFile(instanceId: string): Promise<string | undefined> {
+  const wordlistUrl = core.getInput('wordlistUrl');
+  if (!wordlistUrl) {
+    return;
+  }
+
+  core.info('Uploading wordlist...');
+  const wordlistPath = await downloadFile('wordlist.txt', wordlistUrl);
+  const resp = await execCmd(
+    `corellium image create --project ${process.env.PROJECT} --instance ${instanceId} --format json wordlist.txt mast-wordlist plain ${wordlistPath}`,
+  );
+  const uploadedWordlistResp = tryJsonParse(resp) as unknown as { id: string }[];
+  const wordlistId = uploadedWordlistResp?.[0].id;
+  core.info(`Uploaded wordlist: ${wordlistId}`);
+  return wordlistId;
+}
+
+async function downloadInputFile(): Promise<{ inputsFilePath: string; inputsTimeout: number }> {
+  const inputsFilePath = await downloadFile('inputs.json', core.getInput('inputUrl'));
+
+  // estimating time it takes to execute device inputs - has 10s buffer
+  const inputsJson = JSON.parse(fs.readFileSync(inputsFilePath, 'utf-8'));
+  const inputsTimeout = inputsJson.reduce((acc: number, curr: { wait?: number; duration?: number }) => {
+    if (curr.wait) {
+      acc += curr.wait;
+    }
+    if (curr.duration) {
+      acc += curr.duration;
+    }
+    return acc;
+  }, 10000);
+
+  return { inputsFilePath, inputsTimeout };
+}
+
+export async function pollAssessmentForStatus(
+  assessmentId: string,
+  instanceId: string,
+  expectedStatus: string,
+): Promise<string> {
+  const getAssessmentStatus = async (): Promise<string> => {
+    const resp = await execCmd(`corellium mast get-assessment --instance ${instanceId} --assessment ${assessmentId}`);
+    return (tryJsonParse(resp) as { status: string })?.status;
+  };
+
+  let actualStatus = await getAssessmentStatus();
+  while (actualStatus !== expectedStatus) {
+    await wait();
+    actualStatus = await getAssessmentStatus();
+    if (actualStatus === 'failed') {
+      throw new Error('MAST automated test failed!');
+    }
+  }
+
+  return actualStatus;
+}
+
+async function storeReportInArtifacts(report: string): Promise<void> {
+  const workspaceDir = process.env.GITHUB_WORKSPACE as string;
+  const reportPath = path.join(workspaceDir, 'report.html');
+  fs.writeFileSync(reportPath, report);
+
+  const artifact = new DefaultArtifactClient();
+
+  const { id } = await artifact.uploadArtifact('mast-report', ['./report.html'], workspaceDir);
+  if (!id) {
+    throw new Error('Failed to upload MAST report artifact!');
+  }
+
+  const { downloadPath } = await artifact.downloadArtifact(id);
+
+  core.setOutput('report', downloadPath);
+}
+
+function validateInputsAndVars(): void {
+  if (!process.env.API_TOKEN) {
+    throw new Error('Environment secret missing: API_TOKEN');
+  }
+  if (!process.env.PROJECT) {
+    throw new Error('Environment secret missing: PROJECT');
+  }
+
+  // inputs from action file are not validated https://github.com/actions/runner/issues/1070
+  const requiredInputs = ['flavor', 'os', 'appUrl', 'inputUrl'];
+  requiredInputs.forEach((input: string) => {
+    const inputResp = core.getInput(input);
+    if (!inputResp || typeof inputResp !== 'string' || inputResp === '') {
+      throw new Error(`Input required and not supplied: ${input}`);
+    }
+  });
+
+  const urlInputs = ['appUrl', 'inputUrl', 'wordlistUrl'];
+  urlInputs.forEach((urlInput: string) => {
+    const url = core.getInput(urlInput);
+    if (url) {
+      try {
+        new URL(url);
+      } catch (_) {
+        throw new Error(`Provided URL is invalid: ${urlInput}`);
+      }
+    }
+  });
+}
+
+async function downloadFile(fileName: string, url: string): Promise<string> {
+  const workspaceDir = process.env.GITHUB_WORKSPACE as string;
+  const downloadPath = path.join(workspaceDir, fileName);
+  await exec(`curl -L -o ${downloadPath} ${url}`, [], { silent: true });
+  return downloadPath;
+}
+
+async function wait(ms = 3000): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function execCmd(cmd: string): Promise<string> {
+  let err = '';
+  let resp = '';
+
+  await exec(cmd, [], {
+    silent: true,
+    ignoreReturnCode: true,
+    listeners: {
+      stdout: (data: Buffer) => {
+        resp += data;
+      },
+      stderr: (data: Buffer) => {
+        err += data.toString();
+      },
+    },
+  });
+  if (err) {
+    throw new Error(`Error occurred executing ${cmd}! err=${err}`);
+  }
+  return resp;
+}
+
+function tryJsonParse(jsonStr: string): Record<string, unknown> | undefined {
+  let obj = undefined;
+
+  if (jsonStr && typeof jsonStr === 'string' && jsonStr !== '') {
+    try {
+      obj = JSON.parse(jsonStr);
+    } catch (err) {
+      // do nothing
+    }
+  }
+
+  return obj;
 }
